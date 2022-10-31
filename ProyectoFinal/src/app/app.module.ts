350709deac2ba8426aa0af6d32f8e8c8c925ab1c
MiguelAmezquita <josemiguelamezquita@hotmail.com>
2022-10-30 22:37:05 -0600

    Rutas casi terminado

--- ProyectoFinal/src/app/app.module.ts
+++ ProyectoFinal/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthRoutingModule } from './modules/auth/auth-routing.module';
 
 
 @NgModule({
